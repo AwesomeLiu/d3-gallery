8a02e5b1c9912a2cd03f27a0b8c992c66e368fe5
AwesomeLiu <ryanmartinbornin1996@gmail.com>
2019-07-01 14:55:17 +0800

    update carousel

--- src/router/dashboard/index.js
+++ src/router/dashboard/index.js
@@ -32,42 +32,6 @@ const gallery = [
     "id": 7,
     "url": "http://mm/nn/pp",
   },
-  {
-    "id": 8,
-    "url": "http://xxx/yyy/zzz",
-  },
-  {
-    "id": 9,
-    "url": "http://aaaa/bbbb/cccc",
-  },
-  {
-    "id": 10,
-    "url": "http://111/222/3333",
-  },
-  {
-    "id": 11,
-    "url": "http://abcd/efg/hijk/lmn",
-  },
-  {
-    "id": 12,
-    "url": "http://mm/nn/pp/qq",
-  },
-  {
-    "id": 13,
-    "url": "http://mm/nn/pp",
-  },
-  {
-    "id": 14,
-    "url": "http://mm/nn/pp",
-  },
-  {
-    "id": 15,
-    "url": "http://mm/nn/pp",
-  },
-  {
-    "id": 16,
-    "url": "http://mm/nn/pp",
-  },
 ];
 
 class Dashboard extends Component {
